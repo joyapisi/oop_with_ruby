6bf54bcce04afdef2a5579658e5fab8445fb1eb3
Joy Phoebe <joyapisi@gmail.com>
2023-06-21 16:25:18 +0300

    add animal class and its subClasses

--- animal.rb
+++ animal.rb
@@ -9,35 +9,35 @@
 # animal_1.speak
 # animal_2.speak
 
-require "remover.rb"
-require './foods.rb'
+require 'remover'
+require './foods'
 
 class Animal
   # setters and getters
   attr_accessor :name
   attr_reader :id, :type, :number_of_legs
-  
+
   def initialize(type, number_of_legs, name = 'Unknown')
     @id = Random.rand(1..1000)
     @name = name
     @number_of_legs = number_of_legs
     @type = type
-    @liked_food = NoFood.new()
+    @liked_food = NoFood.new
   end
 
-# a method to tell if the animal likes a type of food or not.
+  # a method to tell if the animal likes a type of food or not.
   def likes_food?(food)
     @liked_food.is_liked?(food)
   end
 
-# all animals say this 
+  # all animals say this
   def speak
     'grrr'
   end
 
-# remove one leg using remover parent class
+  # remove one leg using remover parent class
   def remove_leg
-    remover = Remover.new()
+    remover = Remover.new
     @number_of_legs = remover.decrease(@number_of_legs)
   end
 end
