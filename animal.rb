252b2debb867118fe962b08c03cb73894af28605
Joy Phoebe <joyapisi@gmail.com>
2023-06-21 16:43:53 +0300

    -

--- animal.rb
+++ animal.rb
@@ -1,14 +1,3 @@
-#   def speak
-#     "Bla bla bla"
-#   end
-# end
-
-# animal_1 = Animal.new(4, "Rex")
-# animal_2 = Animal.new(8)
-
-# animal_1.speak
-# animal_2.speak
-
 require 'remover'
 require './foods'
 
